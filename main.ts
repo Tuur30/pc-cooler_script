796652daa699bdbc59c0ba0d247e3d7ecaca6085
Tuur30 <101500547+Tuur30@users.noreply.github.com>
2024-01-23 16:04:38 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,15 @@
-function Serieel_schrijf () {
+function SerialWrite () {
     serial.writeValue("Celcius", temperatuur_celcius)
     serial.writeValue("Kelvin", temperatur_kelvin)
     serial.writeValue("Farenheit", temperatuur_farenheit)
     serial.writeValue("Aan/Uit", Fan_aanuit)
 }
-function Bereken_temperaturen () {
+function CalculateTemperatures () {
     temperatuur_farenheit = input.temperature() * 1.8 + 32
     temperatur_kelvin = 273.15 + input.temperature()
     temperatuur_celcius = input.temperature()
 }
-function Vergelijk () {
+function Compare () {
     if (Start_temperatuur + 5 <= temperatuur_celcius) {
         pins.analogWritePin(AnalogPin.P0, 1023)
         Fan_aanuit = 1
@@ -26,7 +26,7 @@ let Start_temperatuur = 0
 Start_temperatuur = input.temperature()
 Fan_aanuit = 0
 basic.forever(function () {
-    Bereken_temperaturen()
-    Serieel_schrijf()
-    Vergelijk()
+    CalculateTemperatures()
+    SerialWrite()
+    Compare()
 })
